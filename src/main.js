bcb13c250da3fc1dbd422e719bc6c8254298e5c2
Sebastian Kliś <klis.seb@gmail.com>
2025-05-10 13:47:40 +0200

    setup a function to create answer buttons

--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import './sass/main.scss';
 
-const sliderBox = document.querySelector('.hero__slider-box');
-const slider = document.querySelector('.hero__slider');
-const question = document.querySelector('.question');
-const btnArea = document.querySelector('.btnArea');
+const question = document.querySelector('#question');
+const btnArea = document.querySelector('#button-area');
 
+const addButton = (buttonAnswer, followUpQuestion) => {
+	return `<button class="expert-system__button" onclick = "${followUpQuestion}()">${buttonAnswer}</button>`;
+};
+
+function fQuestion0() {
+	question.innerHTML = "Leeet's go, first question";
+	btnArea.innerHTML = `${addButton('Yes', 'fQuestion1')} ${addButton('No', 'fQuestion1')}`;
+}
+
+function fQuestion1() {
+	return console.log('ok');
+}
+
+window.fQuestion0 = fQuestion0;
+window.fQuestion1 = fQuestion1;
+
+/* unused parts of code */
+
+/*
 const carouselWidth = 600;
 
 let index = 0;
@@ -14,19 +31,7 @@ const changeItem = () => {
 		index++;
 	}
 	sliderBox.style.transform = `translateX(${-index * carouselWidth}px)`;
-};
+}; 
 
-function fQuestion0() {
-	question.innerHTML = "Leeet's go, first question";
-	btnArea.innerHTML =
-		"<button onclick = 'fQuestion1();'> Yes </button> <button onclick = 'fQuestion2();'> No </button>";
-}
-
-window.fQuestion0 = fQuestion0;
 window.changeItem = changeItem;
-
-// fQuestion0()
-
-// console.log('hello');
-
-// slider.addEventListener('click', changeItem);
+*/
